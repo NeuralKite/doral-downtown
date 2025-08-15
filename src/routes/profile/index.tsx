41846cb5326b1d923eeefc2ebf26d6c3f3e0997e
NeuralKite <sricardodev@gmail.com>
2025-08-15 00:37:11 -0500

    Fix Landing Page Error - Correct TestimonialsSection

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -2,9 +2,5 @@ import { createFileRoute } from '@tanstack/react-router';
 import UserProfile from '../../components/profile/UserProfile';
 
 export const Route = createFileRoute('/profile/')({
-  component: ProfilePage,
+  component: UserProfile,
 });
-
-function ProfilePage() {
-  return <UserProfile />;
-}
